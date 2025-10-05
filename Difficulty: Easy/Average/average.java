8c0f9ab5493575be1f4e0e212bdbe76b4117ee99
Omprakash <omprakasht5689@gmail.com>
2025-10-05 23:44:50 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Average/average.java
@@ -0,0 +1,20 @@
+class Solution {
+    public static double posAverage(int[] arr) {
+        // Code here
+       double sum = 0;
+        int count = 0;
+
+        for (int num : arr) {
+            if (num >= 0) {
+                sum += num;
+                count++;
+            }
+        }
+
+        if (count == 0) {
+            return 0;
+        }
+
+        return sum / count;
+    }
+}
